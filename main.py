6b20f5ca7ec5e2ad118d1b34c62b7dbf96e304f1
Jeffrey Burke <jburke0519@gmail.com>
2020-01-28 10:41:02 -0500

    Added -points command

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from twitchio.ext import commands
 import json
-from impVar import OAUTH, ID
+import os
+import time
 
 class Bot(commands.Bot):
+    OAUTH = os.environ['TWITCH_OAUTH']
+    ID = os.getenv('TWITCH_ID')
+    print(OAUTH, ID)
 
     def __init__(self):
-        super().__init__(irc_token=OAUTH, client_id=ID, nick='boxingb0t', prefix='-',
+        super().__init__(irc_token=self.OAUTH, client_id=self.ID, nick='boxingb0t', prefix='-',
                          initial_channels=['Toxic_HoneyBadger'])
 
     # Events don't need decorators when subclassed
@@ -29,11 +33,12 @@ class Bot(commands.Bot):
     @commands.command(name='box')
     async def box(self, ctx, challenger, wager):
         wager = int(wager)
-        if wager > 9:
+        if wager >= 10:
             await ctx.send_me(f'{ctx.author.name} has challenged {challenger} for {wager} coins!')
-            await ctx.send_me(f'{challenger} please type -accept to box {ctx.author.name} for {wager} coins!')
+            time.sleep(2)
+            await ctx.send_me( f'{challenger} please type -accept to box {ctx.author.name} for {wager} coins!')
         else:
-            await ctx.send(f'{ctx.author.name} you must challenge a person for 10 coins or more')
+            await ctx.send(f'{ctx.author.id} you must challenge a person for 10 coins or more')
     @box.error
     async def bot_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
@@ -42,11 +47,18 @@ class Bot(commands.Bot):
     @commands.command(name='accept')
     async def accept(self, ctx):
         if box.challenger == ctx.author.name:
-            await ctx.send_me(f'{ctx.author.name} has challenged {challenger} for {wager} coins!')
-            await ctx.send_me(f'{challenger} please type -accept to box {ctx.author.name} for {wager} coins!')
+            await ctx.send_me(f'{ctx.author.id} has challenged {challenger} for {wager} coins!', f'{challenger} please type -accept to box {ctx.author.name} for {wager} coins!')
 
-    async def add_user(slf, author, users):
-        if not author.id in users:
+    @commands.command(name='points')
+    async def points(self, ctx):
+        with open('points.json', 'r') as f:
+            users = json.load(f)
+        points = users[f"{ctx.author.id}"]["points"]
+        print(points)
+        await ctx.send(f'{ctx.author.name} have {points} points')
+    
+    async def add_user(self, author, users):
+        if not f"{author.id}" in users:
             users[author.id] = {}
             users[author.id]['points'] = 100
             
