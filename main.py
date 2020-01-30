8f3d018b8ae6e756c3707d469f6cf2bf0f16af67
Jeffrey Burke <jburke0519@gmail.com>
2020-01-29 22:17:31 -0500

    Beta finished

--- main.py
+++ main.py
@@ -33,41 +33,66 @@ class Bot(commands.Bot):
     # Commands use a decorator...
     @commands.command(name='box')
     async def box(self, ctx, challenger, wager):
+        with open('points.json', 'r') as f:
+            users = json.load(f)
+        self.challenger = challenger
+        self.boxer = ctx.author
         wager = int(wager)
+        self.wager = wager
         if wager >= 10:
-            await ctx.send_me(f'{ctx.author.name} has challenged {challenger} for {wager} coins!')
-            time.sleep(2)
-            await ctx.send_me( f'{challenger} please type -accept to box {ctx.author.name} for {wager} coins!')
+            if users[f"{ctx.author.id}"]["points"] >= wager:
+                await ctx.send_me(f'{ctx.author.name} has challenged {challenger} for {wager} coins!')
+                time.sleep(2)
+                await ctx.send_me(f'{challenger} please type -accept to box {ctx.author.name} for {wager} coins!')
+            else:
+                ctx.send_me(f"{ctx.author.name} you need more points, do -points to see how many you have!")
         else:
             await ctx.send(f'{ctx.author.name} you must challenge a person for 10 coins or more')
+        with open('points.json', 'w') as f:
+            users = json.dump(users, f)
     @box.error
     async def bot_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send('Syntax Error: -box {challeneger} {wager amount}')
+            await ctx.send('Syntax Error: -box {challenger} {wager amount}')
 
     @commands.command(name='accept')
     async def accept(self, ctx):
         match = random.randint(1, 2)
+        challenger = self.challenger
+        boxer = self.boxer
+        challenger = challenger.lower()
+        wager = self.wager
+        with open('points.json', 'r') as f:
+            users = json.load(f)
         if challenger == ctx.author.name:
-            if match == 1:
-                await ctx.send_me(f'{ctx.author.name} has lost against {challenger} and lost {wager} coins!')
-                points = users[f"{challenger}"]["points"]
-                points += wager
+            if users[f"{ctx.author.id}"]["points"] >= wager:
+                with open('points.json', 'r') as f:
+                    users = json.load(f)
+                if match == 1:
+                    await ctx.send_me(f'{ctx.author.name} has lost against {boxer.name} and lost {wager} coins!')
+                    print(f"{boxer.id} won {wager}")
+                    users[f"{boxer.id}"]["points"] += wager
+                    users[f"{ctx.author.id}"]["points"] -= wager
+                else:
+                    await ctx.send_me(f'{ctx.author.name} has won against {boxer.name} and gained {wager} coins!')
+                    print(f"{ctx.author.id} won {wager}")
+                    users[f"{ctx.author.id}"]["points"] += wager
+                    users[f"{boxer.id}"]["points"] -= wager
+                with open('points.json', 'w') as f:
+                    users = json.dump(users, f)
             else:
-                await ctx.send_me(f'{ctx.author.name} has won against {challenger} and gained {wager} coins!')
-                points = users[f"{ctx.author.id}"]["points"]
-                points += wager
-                
+                await ctx.send(f'{ctx.author.name} you must challenge a person for 10 coins or more')
         else:
-            pass
+            print("FAIL ON USER ACCEPT")
+
+        
 
     @commands.command(name='points')
     async def points(self, ctx):
         with open('points.json', 'r') as f:
             users = json.load(f)
         points = users[f"{ctx.author.id}"]["points"]
-        print(points)
-        await ctx.send(f'{ctx.author.name} have {points} points')
+        await ctx.send(f'{ctx.author.name} has {points} points')
     
     async def add_user(self, author, users):
         if not f"{author.id}" in users:
